270b8b4f54f8c77d804898c045460c239037a574
durganp1 <durganp1@gmail.com>
2020-09-28 23:38:57 -0500

    add schema file

---
+++ db/schema.sql
@@ -0,0 +1,39 @@
+
+
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS managers;
+
+CREATE TABLE managers (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL
+);
+
+
+CREATE TABLE departments (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    salary INTEGER UNSIGNED NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+
+CREATE TABLE employees (
+    id INTEGER PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id)
+);
+
