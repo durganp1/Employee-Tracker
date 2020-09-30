cb033bb2bbf36c8172e6b8c8afb57c71b9858ecb
durganp1 <durganp1@gmail.com>
2020-09-30 01:42:49 -0500

    added tables into db and data into tables

--- db/schema.sql
+++ db/schema.sql
@@ -14,16 +14,19 @@ CREATE TABLE managers (
 
 CREATE TABLE departments (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
+    name VARCHAR(50) NOT NULL,
+    manager_id INTEGER NOT NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id)
+    REFERENCES managers(id)
 );
 
 
 CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
+    title VARCHAR(50) UNIQUE NOT NULL,
     salary INTEGER UNSIGNED NOT NULL,
-    department_id INTEGER,
-    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INTEGER NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id)
 );
 
 
@@ -31,9 +34,10 @@ CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id)
+    manager_id INTEGER NOT NULL,
+    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id),
+    role_id INTEGER NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id)
 );
+
 
